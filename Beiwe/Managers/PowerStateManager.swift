bf18aa2da3d766b3882e2f4dbb4c0e39e09e3fea
Keary Griffin <keary@mindless.com>
2016-04-03 00:21:35 -0400

    Fix deprecated code in SwiftyRSA

---
+++ Beiwe/Managers/PowerStateManager.swift
@@ -0,0 +1,9 @@
+//
+//  PowerStateManager.swift
+//  Beiwe
+//
+//  Created by Keary Griffin on 4/1/16.
+//  Copyright © 2016 Rocketfarm Studios. All rights reserved.
+//
+
+import Foundation
---
+++ Beiwe/Managers/ProximityManager.swift
@@ -0,0 +1,9 @@
+//
+//  ProximityManager.swift
+//  Beiwe
+//
+//  Created by Keary Griffin on 4/2/16.
+//  Copyright © 2016 Rocketfarm Studios. All rights reserved.
+//
+
+import Foundation
