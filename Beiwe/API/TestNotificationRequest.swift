3958dfe3e941ac259ecfeb8f8a7cade2232bb6b1
tjaeni01 <tuckerjaenicke@gmail.com>
2020-06-02 09:48:01 -0400

    minor config changes

---
+++ Beiwe/API/TestNotificationRequest.swift
@@ -0,0 +1,28 @@
+//
+//  TestNotificationRequest.swift
+//  Beiwe
+//
+//  Created by Tucker Jaenicke on 5/18/20.
+//  Copyright © 2020 Rocketfarm Studios. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+struct TestNotificationRequest : Mappable, ApiRequest {
+
+    static let apiEndpoint = "/send_survey_notification"
+    typealias ApiReturnType = Survey;
+
+    init() {
+    }
+
+    init?(map: Map) {
+
+    }
+
+    // Mappable
+    mutating func mapping(map: Map) {
+    }
+
+}
