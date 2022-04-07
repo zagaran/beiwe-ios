dc172a66f9b4737186f5851a9936b1532564de04
Eli Jones <biblicabeebli@gmail.com>
2022-04-07 17:38:27 -0400

    [eli-fixes] appears to be a core library change, new parameter names for uicolor objects.

--- Beiwe/Util/AppColors.swift
+++ Beiwe/Util/AppColors.swift
@@ -9,14 +9,15 @@
 import Foundation
 
 class AppColors {
-    static let gradientTop = UIColor(1, g: 118, b: 129, a: 1);
-    static let gradientBottom = UIColor(82, g: 191, b: 190, a: 1);
+    // this code used an older integer version of uicolor, the below is simply a conversion to floats.
+    static let gradientTop = UIColor(red: 1.0/255.0, green: 118.0/255.0, blue: 129.0/255.0, alpha: 1);
+    static let gradientBottom = UIColor(red: 82.0/255.0, green: 191.0/255.0, blue: 190.0/255.0, alpha: 1);
 
-    static let Beiwe1 = UIColor(83, g: 157, b: 166, a: 1)
-    static let Beiwe2 = UIColor(2, g: 115, b: 114, a: 1)
-    static let Beiwe3 = UIColor(0, g: 64, b: 64, a: 1)
-    static let Beiwe4 = UIColor(217, g: 106, b: 42, a: 1)
-    static let Beiwe5 = UIColor(217, g: 88, b: 41, a: 1)
+    static let Beiwe1 = UIColor(red: 83.0/255.0, green: 157.0/255.0, blue: 166.0/255.0, alpha: 1)
+    static let Beiwe2 = UIColor(red: 2.0/255.0, green: 115.0/255.0, blue: 114.0/255.0, alpha: 1)
+    static let Beiwe3 = UIColor(red: 0.0/255.0, green: 64.0/255.0, blue: 64.0/255.0, alpha: 1)
+    static let Beiwe4 = UIColor(red: 217.0/255.0, green: 106.0/255.0, blue: 42.0/255.0, alpha: 1)
+    static let Beiwe5 = UIColor(red: 217.0/255.0, green: 88.0/255.0, blue: 41.0/255.0, alpha: 1)
 
     static let highlightColor = Beiwe2
     static let tintColor = Beiwe2
